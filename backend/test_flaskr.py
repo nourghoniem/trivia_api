031d52a2029d9f235b01251cebd7607ef3847c60
nourghoniem <61252453+nourghoniem@users.noreply.github.com>
2021-07-04 21:32:23 +0200

    Add files via upload

---
+++ backend/test_flaskr.py
@@ -0,0 +1,192 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_path = "postgresql://postgres:123noony@localhost:5432/trivia_test"
+        setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question' : 'how many oscars did leonardo dicaprio win?',
+            'answer' : 'One',
+            'category': 'Entertainment',
+            'difficulty': 2,
+        }
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+    
+    def test_categories_bad_request(self):
+        res = self.client().get('/categories/8')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+        
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_beyond_valid_request(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/7')
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == 7).one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertEqual(question, None)
+
+    def test_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+
+    def test_insert_new_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        
+    def test_method_not_allowed(self):
+        res = self.client().post('/questions/2', json=self.new_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
+
+    def test_search_questions(self):
+        res = self.client().post('/questions', json={'search': 'oscars'})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_search_word_without_results(self):
+        res = self.client().post('/questions', json={'search': 'udacity'})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'], 0)
+        
+    def test_get_questions_by_categories(self):
+        res = self.client().get('/categories/2/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['current_category'])
+
+    def test_category_does_not_exist(self):
+        res = self.client().get('/categories/30/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'bad request')
+
+    def test_quizzes(self):
+        res = self.client().post('/quizzes', json={'previous_questions': [11,12], 'category': {'id': "2", 'type': 'Art'}})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+    
+    def test_quizzes_no_category(self):
+         res = self.client().post('/quizzes', json={})
+         data = json.loads(res.data)
+         self.assertEqual(res.status_code, 400)
+         self.assertEqual(data['success'], False)
+         self.assertEqual(data['message'], 'bad request')
+
+   
+
+       
+
+
+
+
+
+    
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+    
+
+
+
+
+
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
